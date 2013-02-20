549b8e59cfe2037869b2ee3b87dae3634219fa3a
marcodings <marco@dings-its.com>
2013-02-20 17:15:29 +0100

    fix none working "array_sort", refactored to "issue_array_sort" and accompanying issue_compare function.

--- views/j2mantis/view.html.php
+++ views/j2mantis/view.html.php
@@ -19,50 +19,52 @@ jimport( 'joomla.application.component.view');
  
 class J2MantisViewj2mantis extends JView
 {
+	var $sort_on;
+	var $sort_order;
 
-	function array_sort($array, $on, $order='SORT_DESC') 
-    { 
-      $new_array = array(); 
-      $sortable_array = array(); 
-  
-      if (count($array) > 0) { 
-          foreach ($array as $k => $v) { 
-              if (is_array($v)) { 
-                  foreach ($v as $k2 => $v2) { 
-                      if ($k2 == $on) { 
-                          $sortable_array[$k] = $v2; 
-                      } 
-                  } 
-              } else { 
-                  $sortable_array[$k] = $v; 
-              } 
-          } 
-  
-          switch($order) 
-          { 
-              case 'SORT_ASC':    
-                  //echo "ASC"; 
-                  asort($sortable_array); 
-              break; 
-              case 'SORT_DESC': 
-                  //echo "DESC"; 
-                  arsort($sortable_array); 
-              break; 
-          } 
-  
-          foreach($sortable_array as $k => $v) { 
-              $new_array[] = $array[$k]; 
-          } 
-      } 
-      return $new_array; 
-    } 
+	function issue_compare($a, $b) {
+		$on=$this->sort_on;
+		if ( in_array( $on, array('priority', 'severity', 'status', 'project', 'handler', 'reporter', 'resolution' ) ) ) {
+			$aa=$a->$on->id;
+			$bb=$b->$on->id;
+		} elseif ( in_array( $on, array( 'summary', 'date_submitted', 'additional_information', 'description' ) ) ) {
+			// add configuration item to make searching case sensative
+			$aa=strtolower($a->$on);
+			$bb=strtolower($b->$on);
+		} else { // 'id', 'sticky'
+			$aa=$a->$on;
+			$bb=$b->$on;
+		}
+		if ($aa == $bb) {
+			return 0;
+		}
+		return ($aa < $bb) ? -$this->sort_order : $this->sort_order;
+	}
+
+	/**
+	 * Sort the issue list
+	 *
+	 * @param $array
+	 * @param string $on : field from IssueData to sort on, 'NONE' implies no sorting
+	 * @param int $order
+	 * @return mixed
+	 */
+	function issue_array_sort($array, $on='last_updated', $order=1)
+	{
+		$this->sort_on=$on;
+		$this->sort_order=$order;
+		if ( (count($array) > 0) && ( $on != 'NONE' ) ) {
+			uasort($array, array($this, 'issue_compare'));
+		}
+		return $array;
+	}
 
     function display($tpl = null)
     {
 		$params = &JComponentHelper::getParams( 'com_j2mantis' );
 		$overview = $params->get('overview');
 		if(empty($overview)){
-			echo "no overview allowd";
+			echo "no overview allowed";
     		return;
 		}
     	
@@ -79,7 +81,7 @@ class J2MantisViewj2mantis extends JView
 		}
 
 		$bugs = $Mantis->getAllBugsOfAllProjects();
-		$bugs = $this->array_sort($bugs, 'last_updated');
+		$bugs = $this->issue_array_sort($bugs, 'summary', 1);
         $this->bugs = $bugs;
         $this->mantis=$Mantis;
 
