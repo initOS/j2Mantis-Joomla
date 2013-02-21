92737b8ac35b97f9ca875be9daa2869a6344094a
marcodings <marco@dings-its.com>
2013-02-21 10:45:51 +0100

    listview refactored to use tooltips for less important information

--- views/j2mantis/tmpl/default.php
+++ views/j2mantis/tmpl/default.php
@@ -3,8 +3,9 @@
 // No direct access
  
 defined('_JEXEC') or die('Restricted access'); ?>
-<?php JHTML::stylesheet('j2mantis.css', 'components/com_j2mantis/assets/'); ?>
-
+<?php
+JHTML::stylesheet('j2mantis.css', 'components/com_j2mantis/assets/');
+JHTML::_('behavior.tooltip'); ?>
 <h1><?php echo $this->caption; ?></h1>
 <table  id="mt_overview">
 <thead>
@@ -16,36 +17,42 @@ defined('_JEXEC') or die('Restricted access'); ?>
 	  <?php echo JText::_('Summary');?>
 	</th>
 	<th>
-	  <?php echo JText::_('Category');?>
-	</th>
-	<th>
-	  <?php echo JText::_('project'); ?>
-	</th>
-	<th>
 	  <?php echo JText::_('last update');?>
 	</th>
 </tr>
 </thead>
-<?php foreach($this->bugs as $bug){ ?>
+<?php
+	$timezone_offset = -date("H",strtotime("Y-m-d",time()));
+	foreach($this->bugs as $bug) {
+		$uxts_last_updated=strtotime($bug->last_updated. " +" . $timezone_offset . " hours");
+		$fmt_date_long="Y/m/d H:i";
+		$fmt_date_short="y M";
+		$last_updated_long 	= date( $fmt_date_long ,$uxts_last_updated);
+		$last_updated_short = date($fmt_date_short ,$uxts_last_updated);
+		$uxts_date_submitted=strtotime($bug->date_submitted. " +" . $timezone_offset . " hours");
+		$date_submitted_long=date( $fmt_date_long ,$uxts_date_submitted);
+		$url="?option=com_j2mantis&amp;view=showbug&amp;bugid=".
+		     base64_encode($this->mantis->encode((string)$bug->id)).
+			 "&amp;Itemid=".
+			 JRequest::getInt('Itemid',0);
+?>
 <tr>
-	<td>
-		<?php echo $bug->status->name ?>
-	</td>
-	<td>
-		<a href="?option=com_j2mantis&amp;view=showbug&amp;bugid=<?php echo base64_encode($this->mantis->encode((string)$bug->id)); ?>&amp;Itemid=<?php echo JRequest::getInt('Itemid',0);?>"><?php echo $bug->summary ?></a>
-	</td>
-	<td>
-		<?php echo $bug->category ?>
-	</td>
-	<td>
-		<?php echo $bug->project->name ?>
-	</td>
-	<td>
-		<?php 
-		$timezone_offset = -date("H",strtotime("Y-m-d",time()));
-		echo date("d.m.Y H:i" ,strtotime($bug->last_updated. " +" . $timezone_offset . " hours"));?>
-	</td>
+	<td> <?php
+		echo JHTML::tooltip(  $bug->category.' ('.$bug->priority->name.')</br>'.
+				              '<b>S:</b> '. $date_submitted_long. '</br>'.
+				              '<b>U:</b> '. $last_updated_long
+							, $bug->project->name, '', $bug->status->name);
+?>	</td>
+	<td> <?php
+		echo JHTML::tooltip(  $bug->description.'</br>'
+			, $bug->project->name, '', $bug->summary, $url);
+?>	</td>
+	<td> <?php
+		echo JHTML::tooltip(  $last_updated_long
+        	, 'last update', '', $last_updated_short)
+?>  </td>
 </tr>
 <?php } ?>
 </table>
+</br>
 <a href="?option=com_j2mantis&amp;view=addbug&amp;Itemid=<?php echo JRequest::getInt('Itemid',0);?>"><?php echo JText::_('Add new bug report');?></a>
