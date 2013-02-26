ea85f80f906f99d4b800c8274263882b4ca39888
marcodings <marco@dings-its.com>
2013-02-26 13:50:19 +0100

    added moduleclass_sfx styling date handling.. correct for CET DST

--- views/j2mantis/tmpl/default.php
+++ views/j2mantis/tmpl/default.php
@@ -1,89 +1,90 @@
 <?php
- 
 // No direct access
- 
 defined('_JEXEC') or die('Restricted access'); ?>
 <?php
 JHTML::stylesheet('j2mantis.css', 'components/com_j2mantis/assets/');
-JHTML::_('behavior.tooltip'); ?>
-<h1><?php echo $this->caption; ?></h1>
-<table  id="mt_overview">
-<thead>
-<tr>
-	<th>
-	  <?php echo JText::_('Status');?>
-	</th>
-	<th>
-	  <?php echo JText::_('Summary');?>
-	</th>
-<?php if ($this->hasactionholders) { ?>
-    <th>
-		<?php echo JText::_('actionholder');?>
-    </th>
-<?php } ?>
-<?php if ($this->hasduedate) { ?>
-	<th>
-	  <?php echo JText::_('due date');?>
-	</th>
-<?php } ?>
-	<th>
-		<?php echo JText::_('last update');?>
-    </th>
-</tr>
-</thead>
-<?php
-	$timezone_offset = -date("H",strtotime("Y-m-d",time()));
-	foreach($this->bugs as $bug) {
-		$uxts_last_updated=strtotime($bug->last_updated. " +" . $timezone_offset . " hours");
-		$fmt_date_long="Y/m/d H:i";
-		$fmt_date_short="d M";
-		$last_updated_long 	= date( $fmt_date_long ,$uxts_last_updated);
-		$last_updated_short = date( $fmt_date_short ,$uxts_last_updated);
-		$uxts_date_submitted=strtotime($bug->date_submitted. " +" . $timezone_offset . " hours");
-		$date_submitted_long=date( $fmt_date_long ,$uxts_date_submitted);
-		$duedate_long=" ";
-		$duedate_short=" ";
-		if ($bug->due_date) {
-			$uxts_duedate=strtotime($bug->due_date. " +" . $timezone_offset . " hours");
-			$duedate_long=date( $fmt_date_long ,$uxts_duedate);
-			$duedate_short=date( $fmt_date_short ,$uxts_duedate);
-		}
-		$url="?option=com_j2mantis&amp;view=showbug&amp;bugid=".
-		     base64_encode($this->mantis->encode((string)$bug->id)).
-			 "&amp;Itemid=".
-			 JRequest::getInt('Itemid',0);
+JHTML::_('behavior.tooltip');
+$timezone_offset = -date("H", strtotime("Y-m-d", time()))+1;
+$fmt_date_long = "Y/m/d H:i";
+$fmt_date_short = "d M";
 ?>
-<tr>
-	<td> <?php
-		echo JHTML::tooltip(  $bug->category.' ('.$bug->priority->name.')</br>'.
-				              '<b>S:</b> '. $date_submitted_long. '</br>'.
-				              '<b>U:</b> '. $last_updated_long
-							, $bug->project->name, '', $bug->status->name);
-?>	</td>
-	<td> <?php
-		echo JHTML::tooltip(  $bug->description.'</br>'
-			, $bug->project->name, '', $bug->summary, $url);
-?>	</td>
-<?php if ($this->hasactionholders) { ?>
-	<td> <?php
-		if ($bug->j2m['actionholderid']) {
-			echo JHTML::tooltip( sprintf("%s (%s)",$bug->j2m['actionholder'],$bug->j2m['actionholderid'])
-				, 'action holder', '', $bug->j2m['actionholder']);
-		}
-?>  </td>
-<?php } ?>
-<?php if ($this->hasduedate) { ?>
-    <td> <?php
-        echo JHTML::tooltip(  $duedate_long
-        , 'due date', '', $duedate_short)
-?>  </td>
-<?php } ?>
-	<td> <?php
-		echo JHTML::tooltip(  $last_updated_long
-        	, 'last update', '', $last_updated_short)
-?>  </td>
-</tr>
-<?php } ?>
-</table>
-</br>
-<a href="?option=com_j2mantis&amp;view=addbug&amp;Itemid=<?php echo JRequest::getInt('Itemid',0);?>"><?php echo JText::_('Add new bug report');?></a>
+<div class="item-list<?php echo $this->moduleclass_sfx ?>">
+    <h1><?php echo $this->caption; ?></h1>
+    <table id="mt_overview">
+        <thead>
+        <tr>
+            <th>
+				<?php echo JText::_('Status');?>
+            </th>
+            <th>
+				<?php echo JText::_('Summary');?>
+            </th>
+			<?php if ($this->hasactionholders) { ?>
+            <th>
+				<?php echo JText::_('actionholder');?>
+            </th>
+			<?php } ?>
+			<?php if ($this->hasduedate) { ?>
+            <th>
+				<?php echo JText::_('due date');?>
+            </th>
+			<?php } ?>
+            <th>
+				<?php echo JText::_('last update');?>
+            </th>
+        </tr>
+        </thead>
+		<?php
+		foreach ($this->bugs as $bug) {
+			$uxts_last_updated 		= strtotime($bug->last_updated . " +" . $timezone_offset . " hours");
+			$last_updated_long 		= date($fmt_date_long, $uxts_last_updated);
+			$last_updated_short 	= date($fmt_date_short, $uxts_last_updated);
+			$uxts_date_submitted 	= strtotime($bug->date_submitted . " +" . $timezone_offset . " hours");
+			$date_submitted_long 	= date($fmt_date_long, $uxts_date_submitted);
+			$duedate_long = " ";
+			$duedate_short = " ";
+			if ($bug->due_date) {
+				$uxts_duedate = strtotime($bug->due_date . " +" . $timezone_offset . " hours");
+				$duedate_long = date($fmt_date_long, $uxts_duedate);
+				$duedate_short = date($fmt_date_short, $uxts_duedate);
+			}
+			$url = "?option=com_j2mantis&amp;view=showbug&amp;bugid=" .
+				base64_encode($this->mantis->encode((string)$bug->id)) .
+				"&amp;Itemid=" .
+				JRequest::getInt('Itemid', 0);
+			?>
+            <tr>
+                <td> <?php
+					echo JHTML::tooltip($bug->category . ' (' . $bug->priority->name . ')</br>' .
+							'<b>S:</b> ' . $date_submitted_long . '</br>' .
+							'<b>U:</b> ' . $last_updated_long
+						, $bug->project->name, '', $bug->status->name);
+					?>    </td>
+                <td> <?php
+					echo JHTML::tooltip($bug->description . '</br>'
+						, $bug->project->name, '', $bug->summary, $url);
+					?>    </td>
+				<?php if ($this->hasactionholders) { ?>
+                <td> <?php
+					if ($bug->j2m['actionholderid']) {
+						echo JHTML::tooltip(sprintf("%s (%s)", $bug->j2m['actionholder'], $bug->j2m['actionholderid'])
+							, 'action holder', '', $bug->j2m['actionholder']);
+					}
+					?>  </td>
+				<?php } ?>
+				<?php if ($this->hasduedate) { ?>
+                <td> <?php
+					echo JHTML::tooltip($duedate_long
+						, 'due date', '', $duedate_short)
+					?>  </td>
+				<?php } ?>
+                <td> <?php
+					echo JHTML::tooltip($last_updated_long
+						, 'last update', '', $last_updated_short)
+					?>  </td>
+            </tr>
+			<?php } ?>
+    </table>
+    </br>
+    <a href="?option=com_j2mantis&amp;view=addbug&amp;Itemid=<?php echo JRequest::getInt('Itemid', 0);?>"><?php echo JText::_('Add new bug report');?></a>
+</div>
--- views/showbug/tmpl/default.php
+++ views/showbug/tmpl/default.php
@@ -1,12 +1,15 @@
 <?php
-
 // No direct access
-
 defined('_JEXEC') or die('Restricted access'); ?>
-<?php JHTML::stylesheet('j2mantis.css', 'components/com_j2mantis/assets/'); ?>
-<div id="j2Mantis">
+<?php JHTML::stylesheet('j2mantis.css', 'components/com_j2mantis/assets/');
+$timezone_offset = -date("H", strtotime("Y-m-d", time()))+1;
+$fmt_date_long = "Y/m/d H:i";
+$fmt_date_short = "d M";
+?>
+<div class="j2m_showbug<?php echo $this->moduleclass_sfx ?>" id="j2Mantis">
     <!-- <a href="?option=com_j2mantis" style="float: right">zurück zur Übersicht</a> -->
     <h2><?php echo JText::_('Action holder & due date');?></h2>
+
     <form method="post"
           action="?option=com_j2mantis&amp;task=editBug&amp;Itemid=<?php echo JRequest::getInt('Itemid', 0);?>"
           enctype="multipart/form-data">
@@ -14,19 +17,19 @@ defined('_JEXEC') or die('Restricted access'); ?>
         <label for="duedate"><?php echo JText::_('Due date');?></label>
 		<?php echo JHTML::_('calendar', $this->due_date, 'due_date', 'dd_id', '%Y-%m-%d', array('class' => 'inputbox', 'size' => '12', 'maxlength' => '10')); ?>
         <br/>
-		<?php if ( isset($this->actionholders) ) { ?>
-        <!--		TODO: get default value -->
+		<?php if (isset($this->actionholders)) { ?>
         <label>Actionholder:</label>
 
         <select name="actionholderId" STYLE="width: 200px">
             <option
-				<?php if ( $this->default_autionholderid ==-1 ) { ?>
+				<?php if ($this->default_autionholderid == -1) { ?>
                     selected
 				<?php } ?>
-                    value="-1">- none -</option>
-			<?php foreach($this->actionholders as $actionholder){ ?>
+                    value="-1">- none -
+            </option>
+			<?php foreach ($this->actionholders as $actionholder) { ?>
             <option
-				<?php if ( $this->default_autionholderid == $actionholder->id ) { ?>
+				<?php if ($this->default_autionholderid == $actionholder->id) { ?>
                     selected
 				<?php } ?>
                     value="<?php echo $actionholder->id; ?>"><?php echo $actionholder->name; ?></option>
@@ -34,7 +37,8 @@ defined('_JEXEC') or die('Restricted access'); ?>
         </select>
         <br/>
 		<?php } ?>
-        <input style="float:right; background-color: lightsteelblue;" type="submit" value="<?php echo JText::_('submit');?>">
+        <input style="float:right; background-color: lightsteelblue;" type="submit"
+               value="<?php echo JText::_('submit');?>">
         <br/>
     </form>
     <h2><?php echo $this->caption; ?></h2>
@@ -47,7 +51,10 @@ defined('_JEXEC') or die('Restricted access'); ?>
 				<?php echo $this->bug->category . " <i>" . JText::_('in') . "</i> " . $this->bug->project->name;?>
             </td>
             <td>
-				<?php echo gmdate("d.m.Y H:i", strtotime($this->bug->last_updated));?>
+				<?php
+					$uxts_last_updated 		= strtotime($this->bug->last_updated . " +" . $timezone_offset . " hours");
+					$last_updated_long 		= date($fmt_date_long, $uxts_last_updated);
+					echo $last_updated_long; ?>
             </td>
         </tr>
         <tr>
@@ -60,12 +67,12 @@ defined('_JEXEC') or die('Restricted access'); ?>
 				<?php echo nl2br($this->bug->description) ?>
             </td>
         </tr>
-		<?php if (!empty($this->additional_info_readonly)) {?>
-			<tr>
-				<td colspan="3">
-					<?php echo $this->additional_info_readonly;nl2br($this->additional_info_readonly); ?>
-				</td>
-			</tr>
+		<?php if (!empty($this->additional_info_readonly)) { ?>
+        <tr>
+            <td colspan="3">
+				<?php echo $this->additional_info_readonly;nl2br($this->additional_info_readonly); ?>
+            </td>
+        </tr>
 		<?php }; ?>
 		<?php if (count($this->bug->attachments) > 0): ?>
         <tr>
@@ -81,13 +88,15 @@ defined('_JEXEC') or die('Restricted access'); ?>
 		<?php endif; ?>
     </table>
     <h2><?php echo JText::_('attach File');?></h2>
+
     <form method="post"
           action="?option=com_j2mantis&amp;task=addFile&amp;Itemid=<?php echo JRequest::getInt('Itemid', 0);?>"
           enctype="multipart/form-data">
         <input type="hidden" name="bugid" value="<?php echo $this->bug->id ?>"/>
         <label for="name">File:</label>
         <input type="file" name="name" size="30" id="name" maxlength="100000"/><br/>
-        <input  style="float:right; background-color: lightsteelblue;" type="submit" value="<?php echo JText::_('submit');?>">
+        <input style="float:right; background-color: lightsteelblue;" type="submit"
+               value="<?php echo JText::_('submit');?>">
     </form>
 	<?php //var_dump($this->bug); ?>
 	<?php
@@ -101,7 +110,11 @@ defined('_JEXEC') or die('Restricted access'); ?>
                 <td class="author">
 					<?php echo $note->reporter->real_name; ?>
                     <br/>
-                    <span><?php echo gmdate("d.m.Y H:i", strtotime($note->date_submitted));?></span>
+                    <span>
+					<?php
+						$uxts_submitted 		= strtotime($note->date_submitted . " +" . $timezone_offset . " hours");
+						$last_submitted 		= date($fmt_date_long, $uxts_submitted);
+						echo $last_submitted; ?>
                 </td>
                 <td>
 					<?php echo nl2br($note->text); //var_dump($note)?>
@@ -110,22 +123,23 @@ defined('_JEXEC') or die('Restricted access'); ?>
 			<?php } ?>
         </table>
 		<?php } ?>
-
 	<?php if ($this->bug->status->id < 80) { /* FIXME: check if closed */ ?>
     <br/>
     <h2><?php echo JText::_('add Note');?></h2>
     <form method="post"
           action="?option=com_j2mantis&amp;task=addNote&amp;Itemid=<?php echo JRequest::getInt('Itemid', 0);?>">
         <input type="hidden" name="bugid" value="<?php echo $this->bug->id ?>"/>
-        <label for="name"><?php echo JText::_('Name'); if ( $this->fo_name == 1 ) echo '*';?></label>
-        <input type="text" name="name" id="name" <?php if ( $this->fo_name == 1 ) echo 'class="required"'; if ( ! $this->fo_nameedit ) echo ' readonly'; ?>
+        <label for="name"><?php echo JText::_('Name'); if ($this->fo_name == 1) echo '*';?></label>
+        <input type="text" name="name"
+               id="name" <?php if ($this->fo_name == 1) echo 'class="required"'; if (!$this->fo_nameedit) echo ' readonly'; ?>
 			<?php if (!empty($this->user->name)) {
 			echo 'value="' . $this->user->name . ' [' . $this->user->username . ']"';
 		} ?>  />
         <br/>
         <label for="text"><?php echo JText::_('Note');?></label>
         <textarea rows="5" cols="50" name="text" id="text"></textarea>
-        <input  style="float:right; background-color: lightsteelblue; clear:both" type="submit" value="<?php echo JText::_('submit');?>">
+        <input style="float:right; background-color: lightsteelblue; clear:both" type="submit"
+               value="<?php echo JText::_('submit');?>">
     </form>
 	<?php } ?>
     <br/>
